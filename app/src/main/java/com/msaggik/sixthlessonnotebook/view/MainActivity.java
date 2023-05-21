156c01e3720ff78016e56ca14c38bfc4fc5eab3e
Pro2006 <niva11.2006.1@gmail.com>
2023-05-21 23:37:03 +0300

    Третий шаг к величию

--- app/src/main/java/com/msaggik/sixthlessonnotebook/view/MainActivity.java
+++ app/src/main/java/com/msaggik/sixthlessonnotebook/view/MainActivity.java
@@ -9,10 +9,6 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.msaggik.sixthlessonnotebook.R;
-import com.msaggik.sixthlessonnotebook.service.ChatGptService;
-
-import java.io.IOException;
-import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -29,8 +25,6 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
 
         // присваивание id полям
-        imageButton = findViewById(R.id.imageButton);
-        gpt_btn = findViewById(R.id.gpt_btn);
 
         // обработка нажатия кнопки
         imageButton.setOnClickListener(listener);
@@ -43,43 +37,11 @@ public class MainActivity extends AppCompatActivity  {
     private View.OnClickListener listener = new View.OnClickListener()  {
         @Override
         public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.gpt_btn:
-                    ChatGptService service = new ChatGptService(API_KEY);
-
-                    System.out.println("Java CLI application that use Chat GPT API");
-                    System.out.println("---");
-
-                    String queryString = "";
-                    Scanner scanner = new Scanner(System.in);
-
-                    while (true) {
-                        System.out.println();
-                        System.out.println("Enter a string to search for (or /q to quit): ");
-                        queryString = scanner.nextLine();
-
-                        if (queryString.equals("/q")) {
-                            break;
-                        }
-
-                        System.out.println("Searching...");
-                        String answer = null;
-
-                        try {
-                            answer = service.search(queryString);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                        System.out.println(answer);
-                    }
-                case R.id.imageButton:
-
-
                     // переключение на новую активность
                     Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
                     startActivity(intent);
 
             }
-        }
+
     };
 }
