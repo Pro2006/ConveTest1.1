946fd34d8572358add9ab81f99aaf19a0abffcb8
Pro2006 <niva11.2006.1@gmail.com>
2023-05-28 01:56:57 +0300

    Миллионный шаг к успеху, где можно просто ввести токен и пользоваться

---
+++ app/src/main/java/com/msaagik/SixthLessonNotebook/view/MainActivity.java
@@ -0,0 +1,57 @@
+package com.msaagik.SixthLessonNotebook.view;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.msaggik.sixthlessonnotebook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    // создание полей
+    private Button yesButton, noButton;
+
+    private String CHATGPT_TOKEN = "chatgtp_token";
+    private SharedPreferences chatgpt_token_setting;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // присваивание id полям
+        yesButton = findViewById(R.id.yesButton);
+        noButton = findViewById(R.id.noButton);
+        chatgpt_token_setting = getSharedPreferences(CHATGPT_TOKEN, MODE_PRIVATE);
+
+
+        // обработка нажатия кнопки
+        yesButton.setOnClickListener(listener);
+        noButton.setOnClickListener(listener);
+
+
+    }
+
+    // создание слушателя
+    private View.OnClickListener listener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.yesButton) {
+                    // переключение на новую активность
+                    Intent intentSecond = new Intent(getApplicationContext(), SecondActivity.class);
+                    startActivity(intentSecond);}
+            else{
+                    Intent intentToken = new Intent(getApplicationContext(), TokenActivity.class);
+                    startActivity(intentToken);
+
+
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/msaagik/SixthLessonNotebook/view/TokenActivity.java
@@ -0,0 +1,55 @@
+package com.msaagik.SixthLessonNotebook.view;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.msaggik.sixthlessonnotebook.R;
+
+public class TokenActivity extends AppCompatActivity {
+    public String chat_gpt_token;
+    private String CHATGPT_TOKEN = "chatgpt_token";
+    private String TOKEN = "token";
+    private SharedPreferences chatgpt_token_setting;
+
+    private SharedPreferences.Editor chatgpt_token_setting_editor;
+    private Button saveBtn, passBtn;
+    private EditText editToken;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_token);
+
+        saveBtn  = findViewById(R.id.saveBtn);
+        passBtn = findViewById(R.id.passBtn);
+        editToken = findViewById(R.id.editToken);
+        chatgpt_token_setting = getSharedPreferences(CHATGPT_TOKEN, MODE_PRIVATE);
+
+        saveBtn.setOnClickListener(listener);
+        passBtn.setOnClickListener(listener);
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.saveBtn:
+                    chat_gpt_token = editToken.getText().toString();
+                    chatgpt_token_setting_editor = chatgpt_token_setting.edit();
+                    chatgpt_token_setting_editor.putString(TOKEN, chat_gpt_token);
+
+
+            }
+            chatgpt_token_setting_editor.apply();
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(intent);
+        }
+    };
+}
