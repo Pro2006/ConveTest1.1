64fcb53f6d7795e7cc83e9841aa848af9f455ce9
Максим Адаев <maximkad2008@gmail.com>
2023-05-28 21:09:39 +0300

    fix MainActivity
    Теперь если нажал кнопку да, у тебя не будут каждый раз спрашивать, пользовался ли ты ранее

--- app/src/main/java/com/conve/convenote/view/MainActivity.java
+++ app/src/main/java/com/conve/convenote/view/MainActivity.java
@@ -13,51 +13,44 @@ import com.conve.convenote.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    // создание полей
     private Button yesButton, noButton;
-    private String CHATGPT_TOKEN = "chatgtp_token";
-    private String TOKEN = "token";
-    private SharedPreferences chatgpt_token_setting;
-
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // присваивание id полям
         yesButton = findViewById(R.id.yesButton);
         noButton = findViewById(R.id.noButton);
-        chatgpt_token_setting = getSharedPreferences(CHATGPT_TOKEN, MODE_PRIVATE);
-        if (!chatgpt_token_setting.getString(TOKEN, "").equals("")) {
-            Intent intent = new Intent(getApplicationContext(), SecondActivity.class); // переключение обратно в активность демонстрации всех записей
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK); // установления флага экономии ресурсов
+
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        if (sharedPreferences.getBoolean("yesButtonClicked", false)) {
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
 
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("yesButtonClicked", true);
+                editor.apply();
 
-        // обработка нажатия кнопки
-        yesButton.setOnClickListener(listener);
-        noButton.setOnClickListener(listener);
-
-
-    }
-
-    // создание слушателя
-    private View.OnClickListener listener = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View view) {
-            if (view.getId() == R.id.yesButton) {
-                    // переключение на новую активность
-                    Intent intentSecond = new Intent(getApplicationContext(), SecondActivity.class);
-                    startActivity(intentSecond);}
-            else{
-                    Intent intentToken = new Intent(getApplicationContext(), TokenActivity.class);
-                    startActivity(intentToken);
-
-
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
-        }
-    };
+        });
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TokenActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
